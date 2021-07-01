0fc083c80ccaddeaed2d25ef9c7555bc21f86ecc
SY <SY@LAPTOP-PI27NLU6>
2021-07-01 15:39:38 +0900

    시연 대여

---
+++ src/bookproject/borrow/BookBorrowReturn.java
@@ -0,0 +1,291 @@
+package bookproject.borrow;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
+
+import bookproject.DB;
+
+public class BookBorrowReturn extends JFrame implements ActionListener {
+
+	private JPanel p1, p1_1, p1_2, p1_3, p2;
+	private JLabel lbl_Bname, lbl_Bnum, lbl_title, lbl_author, lbl_publisher, lbl_price, lbl_borrow;
+	private JTextField tf_Bname, tf_Bnum, tf_title, tf_author, tf_publisher, tf_price, tf_borrow;
+	private JButton btn_search, btn_br, btn_reset, btn_refresh, btn_back;
+	private Vector<String> con;
+	private DefaultTableModel model;
+	private JTable table;
+	private JScrollPane scrollpane;
+	private String bId;
+	private String bTitle;
+	private String bAuthor;
+	private String bPublisher;
+	private String bPrice;
+	private String bBorrowInfo;
+
+	public JTable getTable() {
+		return table;
+	}
+
+	public BookBorrowReturn(String title, int width, int height) {
+		this.setTitle(title);
+		setSize(width, height);
+		setLocationRelativeTo(this);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		p1 = new JPanel();
+		p1.setLayout(new BorderLayout());
+
+		p1_1 = new JPanel();
+		lbl_Bname = new JLabel("도서명");
+		tf_Bname = new JTextField(13);
+		tf_Bname.addActionListener(this);
+		
+		btn_search = new JButton("검색");
+		btn_search.addActionListener(this);
+
+		p1_1.add(lbl_Bname);
+		p1_1.add(tf_Bname);
+		p1_1.add(btn_search);
+
+		p1.add(p1_1, BorderLayout.NORTH);
+
+		p1_2 = new JPanel();
+		p1_2.setLayout(null);
+		p1_2.setPreferredSize(new Dimension(600, 110)); // 패널 크기 지정
+
+		lbl_Bnum = new JLabel("도서번호");
+		lbl_Bnum.setLocation(100, 20);
+		lbl_Bnum.setSize(60, 20);
+		p1_2.add(lbl_Bnum);
+
+		tf_Bnum = new JTextField();
+		tf_Bnum.setLocation(160, 20);
+		tf_Bnum.setSize(90, 20);
+		tf_Bnum.setEditable(false);
+		tf_Bnum.setBackground(Color.white);
+		p1_2.add(tf_Bnum);
+		
+		lbl_title = new JLabel("제      목");
+		lbl_title.setLocation(330, 20);
+		lbl_title.setSize(60, 20);
+		p1_2.add(lbl_title);
+
+		tf_title = new JTextField();
+		tf_title.setLocation(390, 20);
+		tf_title.setSize(90, 20);
+		tf_title.setEditable(false);
+		tf_title.setBackground(Color.white);
+		p1_2.add(tf_title);
+
+		lbl_author = new JLabel("저      자");
+		lbl_author.setLocation(100, 50);
+		lbl_author.setSize(60, 20);
+		p1_2.add(lbl_author);
+
+		tf_author = new JTextField();
+		tf_author.setLocation(160, 50);
+		tf_author.setSize(90, 20);
+		tf_author.setEditable(false);
+		tf_author.setBackground(Color.white);
+		p1_2.add(tf_author);
+
+		lbl_publisher = new JLabel("출 판 사");
+		lbl_publisher.setLocation(330, 50);
+		lbl_publisher.setSize(60, 20);
+		p1_2.add(lbl_publisher);
+
+		tf_publisher = new JTextField();
+		tf_publisher.setLocation(390, 50);
+		tf_publisher.setSize(90, 20);
+		tf_publisher.setEditable(false);
+		tf_publisher.setBackground(Color.white);
+		p1_2.add(tf_publisher);
+
+		lbl_price = new JLabel("가      격");
+		lbl_price.setLocation(100, 80);
+		lbl_price.setSize(60, 20);
+		p1_2.add(lbl_price);
+
+		tf_price = new JTextField();
+		tf_price.setLocation(160, 80);
+		tf_price.setSize(90, 20);
+		tf_price.setEditable(false);
+		tf_price.setBackground(Color.white);
+		p1_2.add(tf_price);
+
+		lbl_borrow = new JLabel("대출여부");
+		lbl_borrow.setLocation(330, 80);
+		lbl_borrow.setSize(60, 20);
+		p1_2.add(lbl_borrow);
+
+		tf_borrow = new JTextField();
+		tf_borrow.setLocation(390, 80);
+		tf_borrow.setSize(90, 20);
+		tf_borrow.setEditable(false);
+		tf_borrow.setBackground(Color.white);
+		p1_2.add(tf_borrow);
+
+		p1.add(p1_2);
+
+		p1_3 = new JPanel();
+		p1_3.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		btn_br = new JButton("대여/반납");
+		btn_reset = new JButton("초기화");
+		btn_refresh = new JButton("새로고침");
+		btn_back = new JButton("돌아가기");
+
+		p1_3.add(btn_br);
+		p1_3.add(btn_reset);
+		p1_3.add(btn_refresh);
+		p1_3.add(btn_back);
+
+		btn_br.addActionListener(this);
+		btn_reset.addActionListener(this);
+		btn_refresh.addActionListener(this);
+		btn_back.addActionListener(this);
+
+		p1.add(p1_3, BorderLayout.SOUTH);
+
+		Vector<String> column = new Vector<String>();
+		column.addElement("도서번호");
+		column.addElement("제목");
+		column.addElement("저자");
+		column.addElement("출판사");
+		column.addElement("가격");
+		column.addElement("대출여부");
+
+		model = new DefaultTableModel(column, 0) {
+			public boolean isCellEditable(int r, int c) {
+				return false;
+			}
+		};
+
+		makeTable();
+
+		table = new JTable(model);
+		scrollpane = new JScrollPane(table);
+
+		table.getTableHeader().setReorderingAllowed(false);
+
+		p2 = new JPanel();
+		p2.setLayout(new BorderLayout());
+		p2.add(scrollpane);
+
+		add(p1, BorderLayout.NORTH);
+		add(p2, BorderLayout.CENTER);
+		this.setVisible(true);
+
+	}
+
+	private void makeTable() {
+		String sql = "SELECT * FROM LIB ORDER BY LIB_CODE";
+		ResultSet rs = DB.getResultSet(sql);
+
+		try {
+			while (rs.next()) {
+				con = new Vector<String>();
+				bId = rs.getString(1);
+				bTitle = rs.getString(2);
+				bAuthor = rs.getString(3);
+				bPublisher = rs.getString(4);
+				bPrice = rs.getString(5);
+				bBorrowInfo = rs.getString(6);
+
+				con.add(bId);
+				con.add(bTitle);
+				con.add(bAuthor);
+				con.add(bPublisher);
+				con.add(bPrice);
+				con.add(bBorrowInfo);
+				model.addRow(con);
+			}
+		} catch (SQLException e) {
+			System.out.println("해당 드라이버가 없습니다.");
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		DB.init();
+		new BookBorrowReturn("도서대여/반납", 600, 700);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btn_br) {
+			new BorrowReturn("대여/반납", 500, 600, this);
+		}
+
+		if (e.getSource() == btn_search | e.getSource() == tf_Bname) {
+			String strName = tf_Bname.getText();
+			String sql = "SELECT * FROM LIB WHERE LIB_NAME = '" + strName + "'";
+			ResultSet rs = DB.getResultSet(sql);
+
+			if (strName.equals(tf_Bname.getText())) {
+				try {
+					while (rs.next()) {
+						con = new Vector<String>();
+						bId = rs.getString(1);
+						bTitle = rs.getString(2);
+						bAuthor = rs.getString(3);
+						bPublisher = rs.getString(4);
+						bPrice = rs.getString(5);
+						bBorrowInfo = rs.getString(6);
+
+						tf_Bnum.setText(bId);
+						tf_title.setText(bTitle);
+						tf_author.setText(bAuthor);
+						tf_publisher.setText(bPublisher);
+						tf_price.setText(bPrice);
+						tf_borrow.setText(bBorrowInfo);
+					}
+				} catch (SQLException e1) {
+					System.out.println("해당 드라이버가 없습니다.");
+					e1.printStackTrace();
+				}
+			}
+		}
+
+		if (e.getSource() == btn_reset) {
+			model.setNumRows(0);
+		}
+
+		if (e.getSource() == btn_refresh) {
+			model.setNumRows(0);
+			makeTable();
+		}
+
+		if (e.getSource() == btn_back) {
+			System.exit(0);
+		}
+	}
+}
---
+++ src/bookproject/borrow/BorrowReturn.java
@@ -0,0 +1,261 @@
+package bookproject.borrow;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Vector;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.table.DefaultTableModel;
+
+import bookproject.DB;
+
+public class BorrowReturn extends JFrame implements ActionListener {
+
+	private JPanel p1, p1_1, p1_2, p2;
+	private JLabel lbl_rrn, lbl_Bnum, lbl_title;
+	private JTextField tf_rrn, tf_Bnum, tf_title;
+	private JButton btn_borrow, btn_return, btn_cancel;
+	private String[] tmp;
+	private Vector<String> con, str;
+	private DefaultTableModel model;
+	private String bId;
+	private String mName;
+	private String mPhone;
+	private String bName;
+	private String bNum;
+	private String date;
+	private BookBorrowReturn bbr;
+	private String bookNum;
+
+	public BorrowReturn(String title, int width, int height, BookBorrowReturn bbr) {
+		this.bbr = bbr;
+		this.setTitle(title);
+		setSize(width, height);
+		setLocationRelativeTo(this);
+		// setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		p1 = new JPanel();
+		p1.setLayout(new BorderLayout());
+
+		p1_1 = new JPanel();
+
+		lbl_rrn = new JLabel("회원주민번호");
+		tf_rrn = new JTextField(10);
+		btn_borrow = new JButton("대여");
+		btn_borrow.addActionListener(this);
+		btn_return = new JButton("반납");
+		btn_return.addActionListener(this);
+		btn_cancel = new JButton("취소");
+		btn_cancel.addActionListener(this);
+
+		p1_1.add(lbl_rrn);
+		p1_1.add(tf_rrn);
+		p1_1.add(btn_borrow);
+		p1_1.add(btn_return);
+		p1_1.add(btn_cancel);
+		p1.add(p1_1, BorderLayout.NORTH);
+
+		p1_2 = new JPanel();
+
+		lbl_Bnum = new JLabel("도서번호");
+		tf_Bnum = new JTextField(10);
+		lbl_title = new JLabel("제    목");
+		tf_title = new JTextField(10);
+		tf_title.addActionListener(this);
+
+		Vector<String> column = new Vector<String>();
+		column.addElement("대여번호");
+		column.addElement("회원이름");
+		column.addElement("회원전화");
+		column.addElement("도서이름");
+		column.addElement("도서번호");
+		column.addElement("날짜");
+
+		model = new DefaultTableModel(column, 0) {
+			public boolean isCellEditable(int r, int c) {
+				return false;
+			}
+		};
+
+		makeTable();
+
+		p2 = new JPanel();
+		p2.setLayout(new BorderLayout());
+
+		JTable table = new JTable(model);
+		JScrollPane scrollpane = new JScrollPane(table);
+		p2.add(scrollpane);
+
+		p1_2.add(lbl_Bnum);
+		p1_2.add(tf_Bnum);
+		p1_2.add(lbl_title);
+		p1_2.add(tf_title);
+		p1.add(p1_2);
+
+		add(p1, BorderLayout.NORTH);
+		add(p2, BorderLayout.CENTER);
+
+		this.setVisible(true);
+	}
+
+	private void makeTable() {
+		String sql = "SELECT RENT_NO, MEM_NAME, MEM_PHONE, LIB_NAME, LIB_CODE, TO_CHAR(RENT_DATE, 'YYYY-MM-DD') FROM RENT ORDER BY RENT_NO";
+		ResultSet rs = DB.getResultSet(sql);
+
+		try {
+			while (rs.next()) {
+				con = new Vector<String>();
+				bId = rs.getString(1);
+				mName = rs.getString(2);
+				mPhone = rs.getString(3);
+				bName = rs.getString(4);
+				bNum = rs.getString(5);
+				date = rs.getString(6);
+
+				con.add(bId);
+				con.add(mName);
+				con.add(mPhone);
+				con.add(bName);
+				con.add(bNum);
+				con.add(date);
+				model.addRow(con);
+			}
+		} catch (SQLException e) {
+			System.out.println("해당 드라이버가 없습니다.");
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		boolean borrowState = false;
+
+		if (e.getSource() == btn_borrow | e.getSource() == tf_title) {
+			
+			String mbNum = tf_rrn.getText();
+			String libCode = tf_Bnum.getText();
+			String libName = tf_title.getText();
+			
+			libCode = libCode.toUpperCase();
+
+			String rsBName =  "SELECT LIB_NAME "
+		               + "FROM LIB "
+		               + "WHERE LIB_CODE = '" + libCode + "'";
+		    ResultSet rsName = DB.getResultSet(rsBName);
+		    
+		    String bookName = "";
+		    
+		    try {
+				if(rsName.next()) {
+					bookName = rsName.getString(1);
+				}
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		    
+			for (int i = 0; i < bbr.getTable().getRowCount(); i++) {
+				if (libCode.equals(bbr.getTable().getValueAt(i, 0))) {
+					if (bbr.getTable().getValueAt(i, 5).equals("N")) {
+						borrowState = true;
+
+						JOptionPane.showMessageDialog( // 메시지창 출력
+								this, "대여중인 도서입니다.", "ErrorMessage", JOptionPane.ERROR_MESSAGE);
+						tf_Bnum.setText("");
+						tf_title.setText("");
+					}
+
+				}
+			}
+
+			if (borrowState == false && bookName.equals(libName) && !mbNum.equals("") && !libCode.equals("")) {
+
+				String sql = "INSERT INTO JAVAJO.RENT VALUES((SELECT NVL(MAX(RENT_NO)+1,1) FROM JAVAJO.RENT), "
+						+ "(SELECT MB_NAME FROM MEMBERS WHERE MB_NUM = '" + mbNum + "'), "
+						+ "(SELECT MB_PHONE FROM MEMBERS WHERE MB_NUM = '" + mbNum + "'),"
+						+ "(SELECT LIB_CODE FROM LIB WHERE LIB_CODE = '" + libCode + "'),"
+						+ "(SELECT LIB_NAME FROM LIB WHERE LIB_CODE = '" + libCode + "'), TO_DATE(SYSDATE))";
+
+				String sqlState = "UPDATE LIB SET LIB_STATE='N' " + "WHERE LIB_CODE='" + libCode + "'";
+
+				DB.executeQuery(sql); // DB 내용 추가
+				DB.executeQuery(sqlState); // 대출여부 수정
+
+				tf_Bnum.setText("");
+				tf_title.setText("");
+				
+				model.setNumRows(0);
+				makeTable();
+			}
+		}
+		
+		if (e.getSource() == btn_return) {
+			
+			bookNum = "";
+			String mbNum = tf_rrn.getText();
+			String libCode = tf_Bnum.getText();
+			
+			String rsMemNum =  "SELECT MB_NAME "
+		               + "FROM MEMBERS "
+		               + "WHERE MB_NUM = '" + mbNum + "'";
+		    ResultSet rsNum = DB.getResultSet(rsMemNum);
+		    
+		    String memName = "";
+		    
+		    try {
+				if(rsNum.next()) {
+					memName = rsNum.getString(1);
+				}
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		    
+			for (int i = 0; i < model.getRowCount(); i++) {
+				if (memName.equals(model.getValueAt(i, 1))) {
+					bookNum = (String) model.getValueAt(i, 4);
+					}
+				}
+			
+			String sql = "DELETE FROM RENT WHERE MEM_NAME = (SELECT MB_NAME FROM MEMBERS WHERE MB_NUM = '"
+					+ mbNum + "')";
+			
+			String sqlState = "UPDATE LIB SET LIB_STATE='Y' WHERE LIB_CODE='" + bookNum + "'";
+			
+			DB.executeQuery(sql); // DB 내용 삭제
+			DB.executeQuery(sqlState); // 대출여부 수정
+
+			mbNum = "";
+			
+			model.setNumRows(0);
+			makeTable();
+		}
+
+		if (e.getSource() == btn_cancel) {
+			this.dispose();
+		}
+	}
+
+}
